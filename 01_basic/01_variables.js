fd6c3753828401b3e1e8670e1c84743d8a41592e
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-09 20:00:37 +0000

    container update

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const data1 = "It is a constant variable";
+let data2 = "It is a let variable";
+var data3 = "It is a var variable";
+data4 = [1,2,3];
+
+// do not use var because of scope and functional
+
+console.log(typeof(data4));
+// console.table([data1, data2, data3, data4]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // through arror because we are using node js, Not browser
+
+let name = "Sarvender"  //string type
+let age = 24 //number
+let isLoggedIn = false // boolean
+
+// primitive data type
+// number
+// bigint
+// string
+// boolean = true/false
+// null = standalone value 
+// undefined = define but value not assigned
+// symbol => unique
