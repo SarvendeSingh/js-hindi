3423dc6699125444f5d2cec3f255198e5101d29c
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-10 06:16:42 +0000

    container update

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,13 +1,42 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueNumber = String(score);
+// let valueNumber = String(score);
 
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
 
-let scoreNumber = 1
-let newScore = Boolean(scoreNumber);
-console.log(newScore);
+// let scoreNumber = 1
+// let newScore = Boolean(scoreNumber);
+// console.log(newScore);
+
+
+// operations
+
+let value = 3 
+let negValue = -3
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " world!"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" +  2);
+console.log(1 +  "2");
+console.log("1" +  "2");
+console.log("1" +  2 + 2);
+console.log(1 +  2 + "2");
+
+console.log( 3 + 4 * 5 % 3); // do not such complicated code 
+
+
+
+
+
+
+
+
+
