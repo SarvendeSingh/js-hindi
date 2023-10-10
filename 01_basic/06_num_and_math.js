6028a3be9832283f2c46ef9d5c92ddc59c0d8c85
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-10 09:34:50 +0000

    container update

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,25 @@
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,6,3,5,7));
+// console.log(Math.max(4,6,3,5,7));
+// console.log( Math.floor(Math.random() * 5) + 1);
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
