295c412971808802a39cbf72046df9c5563194b6
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-10 05:42:23 +0000

    container update

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = 33
+
+console.log(typeof score);
+
+let valueNumber = String(score);
+
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+
+let scoreNumber = 1
+let newScore = Boolean(scoreNumber);
+console.log(newScore);
