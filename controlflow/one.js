a11623e66a9b388ccc967e782a78f3873bd739bb
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-13 18:49:33 +0000

    operators

--- controlflow/one.js
+++ controlflow/one.js
@@ -2,3 +2,24 @@ let isLoggedIn = true;
 if(isLoggedIn){
 
 }
+
+//nullish coalescing operator (??) : null undefined
+
+let val1;
+
+//val1 = 5 ?? 10;
+//val1 = null ?? 20;
+//val1 = null ?? null;
+//val1 = null ?? undefined;
+// val1 = null ?? [20, 30];
+// val1 = undefined ?? [20, 30];
+// val1 = {name: 'satyam'} ?? [20, 30];
+
+// val1 =  [20, 30] ?? {name: 'satyam'} ;
+
+// val1 =  undefined ?? 45 ;
+// val1 =  undefined ?? '' ;
+// val1 =  2 ?? '' ;
+val1 =  ' ' ?? '' ;
+
+console.log(val1);
