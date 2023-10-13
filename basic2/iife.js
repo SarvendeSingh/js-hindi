daf23091a41da669ba8d542698bc4e71e6d538f5
Sarvender Singh <90505408+SarvendeSingh@users.noreply.github.com>
2023-10-13 12:48:29 +0000

    iife

---
+++ basic2/iife.js
@@ -0,0 +1,9 @@
+//immediately invoked function expressions (iife)
+//some time we want immediately envoke function when our app start and it contect with data base
+//
+
+(
+    function one(){
+        console.log('one');
+    }
+)();
